c9bc32839c5ced9ec8bafdee0d0f8725a519adf2
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2019-08-05 13:28:31 -0600

    jeff stuff

--- Jeff/test.py
+++ Jeff/test.py
@@ -1,31 +1,12 @@
-from tkinter import *
-
-root = Tk()
-root.title('Demo Pypeline')
-
-window_width = 800
-window_height = 600
-
-grid_bg = '#0d0e22'
-grid_lines = '#508ac9'
-
-loop_time = 10
-
-canvas = Canvas(
-    root,
-    width=window_width,
-    height=window_height,
-    bg=grid_bg
-)
-canvas.grid(column=0, row=0)
+import pygame
 
 
-def main_loop():
+def main():
 
-    
-
-    canvas.after(loop_time, main_loop)
+    pygame.init()
 
 
-root.mainloop()
-main_loop()
+print(pygame.get_init())
+
+if __name__ == '__main__':
+    main()
