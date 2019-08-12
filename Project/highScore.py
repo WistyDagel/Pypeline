27cfe8523f022b6408af2180bc20181ed41b860e
DragonGeddon <starcannon237@gmail.com>
2019-08-12 11:44:03 -0600

    Create highScore.py
    made prototype of the highscore still needs refining

---
+++ Project/highScore.py
@@ -0,0 +1,53 @@
+import pygame as pg
+import random
+from os import path
+
+class highScore:
+
+    def load_data(highscore):
+        # load high score
+        self.dir = path.dirname(__file__)
+        with open(path.join(self.dir, HS_FILE), 'r') as f:
+            try:
+                self.highscore = int(f.read())
+            except:
+                self.highscore = 0
+
+    def save_data(highscore):
+        self.dir = path.dirname(__file__)
+        with open(path.join(self.dir, HS_FILE), 'r') as f:
+            try:
+                self.highscore = int(f.read())
+            except:
+                self.highscore = 0
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.all_sprites.draw(self.screen)
+        self.draw_text(str(self.score), 22, WHITE, WIDTH / 2, 15)
+        pg.display.flip()
+
+    def show_start_screen(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("High Score: " + str(self.highscore), 22, WHITE, WIDTH / 2, 15)
+        pg.display.flip()
+        self.wait_for_key()
+
+    def show_go_screen(self):
+        if not self.running:
+            return
+        self.screen.fill(BGCOLOR)
+        self.draw_text("High Score: " + str(self.highscore), 22, WHITE, WIDTH / 2, HEIGHT / 2 + 40)
+        pg.display.flip()
+        self.wait_for_key()
+
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+g.show_start_screen()
+g.show_go_screen()
