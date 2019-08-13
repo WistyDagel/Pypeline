cb242693e65db93162ea5a1a597cbc3546f18d56
DeryGermann <termaiter@gmail.com>
2019-08-13 14:42:22 -0600

    Deleted comments in the PowerUp file

--- Project/Prototype_II/powerUps.py
+++ Project/Prototype_II/powerUps.py
@@ -14,11 +14,6 @@ class PowerUps(square.Square):
         NUKE = color.YELLOW,
         MINE = color.RED,
         PHASE = color.WHITE
-    # def __init__(self, powerupWeight: int, x: int, y: int, c: color):
-    #     self.weight = powerupWeight
-    #     self.x = x
-    #     self.y = y
-    #     self.c = c
 
     def __init__(self, maxX: int, maxY: int, type: Type):
         self.w = 8
@@ -27,15 +22,6 @@ class PowerUps(square.Square):
         self.y = math.ceil(random.randint(0, maxY - self.h / 2))
         self.type = type
         self.color = self.type.value
-
-        # sPowerUp = square.Square(random.randint(0, int(self.x - self.weight + 1)),  # random x
-        #     random.randint(0, int(self.y - self.weight + 1)),  # random y
-        #     self.weight * 1.5,  # 50% larger than the bike
-        #     self.weight * 1.5)  # 50% larger than the bike
-
-        # return sPowerUp
+        
     def collides(self, bike: b.Bike):
-        return self.overlaps(bike.get_leading_edge());
-
-    # def returnColor(self):
-    #     return self.c
+        return self.overlaps(bike.get_leading_edge())
